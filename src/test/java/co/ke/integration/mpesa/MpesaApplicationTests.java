d4e887d100503ccd80dd90167757bfdd9ff79bc4
Muniu Kariuki <muniukariuki@gmail.com>
2024-10-24 14:20:16 +0300

    Initial Commit

---
+++ src/test/java/co/ke/integration/mpesa/MpesaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ke.integration.mpesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
